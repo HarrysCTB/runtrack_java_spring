08a1a3bf2e2e8beaacdc5bfc59c796f0e5a2b9d6
Elarif Coding <71882575+HarrysCTB@users.noreply.github.com>
2023-09-18 16:34:28 +0200

    Add files via upload

---
+++ jour1/src/main/java/com/example/RuntrackJava/RuntrackJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.RuntrackJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuntrackJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuntrackJavaApplication.class, args);
+	}
+
+}
